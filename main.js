a0c161ff8d749e15aed140d17036c04321d694ff
heitorgf <heitor1028@hotmail.com>
2023-10-27 10:41:45 -0300

    final mobile

---
+++ main.js
@@ -0,0 +1,8 @@
+(function(){
+    var $body = document.querySelector('body');
+    var menu = new Menu({
+        container: '.header__nav',
+        toggleBtn: '.header__btnMenu',
+        widthEnabled: 1023 
+    })
+})
